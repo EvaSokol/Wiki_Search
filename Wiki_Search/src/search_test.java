9f7df508d11326480601ab70151975b36023ad72
EvaSokol <esokolyanskaya@lohika.com>
2014-07-04 10:38:43 +0300

    First steps
    Open browser, try to input data

---
+++ Wiki_Search/src/search_test.java
@@ -0,0 +1,14 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class search_test {
+
+	@Test
+	public void test() {
+		start.open();
+		start.find("инкапсул€ци€");
+	}
+
+}
